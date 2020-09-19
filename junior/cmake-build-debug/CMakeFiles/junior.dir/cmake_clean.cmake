5b8f0f731899cc5b05aff44d9c69880a362f4342
YUQIAO ZHAO <zhaoyuqiao97@gmail.com>
2020-09-19 13:15:34 +0200

    新建CLion项目 junior（初级算法）

---
+++ junior/cmake-build-debug/CMakeFiles/junior.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/junior.dir/main.cpp.o"
+  "junior"
+  "junior.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/junior.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
